891b78bf4c827a0109770269b6463ae91d497bd6
srikk <122169588+py-code314@users.noreply.github.com>
2026-05-09 14:08:09 -0400

    fix: Render homepage without folders & files if user not logged in

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,33 +1,37 @@
-
 const { prisma } = require('../lib/prisma.js')
 
 /* Show home page */
 async function home_page_get(req, res, next) {
   try {
-    const userId = req.user.id
-    // Get all folders
-    const folders = await prisma.folder.findMany({
-      where: { 
-        userId,
-        parentId: null
-        
+    // Check if user logged in
+    if (req.user) {
+      const userId = req.user.id
 
-       },
-    })
-    // Get all files in root folder
-    const files = await prisma.file.findMany({
-      where: { 
-        userId,
-        folderId: null
+      // Get all folders
+      const folders = await prisma.folder.findMany({
+        where: {
+          userId,
+          parentId: null,
+        },
+      })
+      // Get all files in root folder
+      const files = await prisma.file.findMany({
+        where: {
+          userId,
+          folderId: null,
+        },
+      })
 
-       },
-    })
-
-    res.render('pages/home', {
-      title: 'Home',
-      folders,
-      files
-    })
+      res.render('pages/home', {
+        title: 'Home',
+        folders,
+        files,
+      })
+    } else { // User not logged in
+      res.render('pages/home', {
+        title: 'Home',
+      })
+    }
   } catch (err) {
     console.error(err)
     return next(err)

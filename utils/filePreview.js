81a967c69fc2b2b94670acdd179df5ccc83910c3
srikk <122169588+py-code314@users.noreply.github.com>
2026-05-15 09:03:41 -0400

    fix: Cleanup code in `fileDetails.ejs`

--- utils/filePreview.js
+++ utils/filePreview.js
@@ -18,10 +18,18 @@ async function generateFilePreview(file) {
   } else if (['aac', 'mid', 'midi', 'mp3', 'wav', 'flac', 'm4p', 'ogg', 'wma'].includes(fileType)) { // Audio files
     previewType = 'audio'
     previewUrl = file.url
-  } else if (['webm', 'mkv', 'flv', 'avi', 'mov', 'wmv', 'rm', 'amv', 'mp4', 'mpg', 'mpeg', 'm4v', '3gp'].includes(fileType)) { // Video files
+  }
+  else if (['webm', 'mkv', 'flv', 'avi', 'mov', 'wmv', 'rm', 'amv', 'mp4', 'mpg', 'mpeg', 'm4v', '3gp'].includes(fileType)) { // Video files
     previewType = 'video'
-    previewUrl = file.url
-  } else { // For .doc and .docx files
+    previewUrl = cloudinary.url(file.storedName, {
+      resource_type: 'video',
+      width: 300,
+      crop: 'scale',
+      start_offset: '1',
+    })
+  }
+  
+  else { // For .doc and .docx files
     previewType = 'icon'
     previewUrl = `images/icons/${fileType}_icon.svg`
   }
